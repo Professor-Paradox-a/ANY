b72cd4fdd7dfe5313cf67ef923c257157e71075f
Professor-Paradox-a <149234096+Professor-Paradox-a@users.noreply.github.com>
2023-11-27 06:26:39 +0000

    resouce

--- ANY/ANY_app1/models.py
+++ ANY/ANY_app1/models.py
@@ -4,6 +4,17 @@ from django.db import models
 class Person(models.Model):
     name =  models.CharField(max_length=30)
     image = models.ImageField(upload_to='images/',blank=True)
+    email = models.EmailField(max_length=40,blank = True)
     
     def __str__(self):
         return self.name
+
+class Resources(models.Model):
+    resource_name = models.CharField(max_length=45)
+    description = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images',blank= True)
+    user = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='resources')
+    
+    def __str__(self):
+        return self.resource_name
+
